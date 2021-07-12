1aba8cc82724172a72792d63ca6b4282e0a28f80
fabio <dasilvadeoliveirafabio@gmail.com>
2021-07-12 10:51:52 -0300

    header parcial

---
+++ src/pages/Home/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+function Header(){
+    return<>
+      <header className="header">
+        <div className="toolbar">
+          <div className="">
+            <span>conecta dev</span>
+          </div>
+          <div className="">
+            <button>Novo Post</button>
+            <span>img1</span>
+            <span>img2</span>
+          </div>
+        </div>
+      </header>
+    </>
+}
+export default Header;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './components/Header';
+
+
+function Home(){
+  return<>
+      <Header />
+      <div></div>
+      <main>
+          <div className="navbar">
+
+          </div>
+          <div className="feed">
+
+          </div>
+      </main>
+  </>
+}
+export default Home;
